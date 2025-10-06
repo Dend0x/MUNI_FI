e699ac4c12a5fbfcb246de76de7da06b0a8830cf
Dend0x <dannyryska05@gmail.com>
2025-10-06 14:22:53 +0200

    Create odpovedniky01.hs

---
+++ ib015/Odpovedniky/odpovedniky01.hs
@@ -0,0 +1,19 @@
+sign :: Integer -> Integer
+sign 0 = 0
+sign x = if x > 0 then 1 else -1
+
+fullAdder :: Bool -> Bool -> Bool -> (Bool, Bool)
+fullAdder False False False = (False, False)
+fullAdder True True True = (True, True)
+fullAdder x y z = if not ((x && y) || (x && z) || (y && z)) then (True, False) else (False, True)
+
+classifyIP :: (Int, Int, Int, Int) -> String
+classifyIP (147, 251, 49, 10) = "IS"
+classifyIP (172, x, _, _) = if x >= 16 && x <= 31 then "FI" else "Other"
+classifyIP (147, 251, 58, _) = "FI"
+classifyIP (147, 251, x, _) = if x >= 42 && x <= 53 then "FI" else "MUNI"
+classifyIP (_, _, _, _) = "Other"
+
+is3DCloser :: (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> Bool
+is3DCloser (x1, y1, z1) (x2, y2, z2) (x3, y3, z3) = distance x1 y1 z1 x2 y2 z2 < distance x1 y1 z1 x3 y3 z3
+    where distance x1 y1 z1 x2 y2 z2 = (x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2
