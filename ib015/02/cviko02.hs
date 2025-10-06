012f9d7059b4d19f0884b2d3914f99fc8848c048
Dend0x <dannyryska05@gmail.com>
2025-10-06 23:16:26 +0200

    Haskell

--- ib015/02/cviko02.hs
+++ ib015/02/cviko02.hs
@@ -51,6 +51,33 @@ mygcd a 0 = a
 mygcd 0 b = b
 mygcd a b = if a > b then mygcd (a - b) b else mygcd a (b - a)
 
+primeDivRec :: Integral a => a -> a -> a
+primeDivRec 1 y = 0
+primeDivRec x y = if (mod x y) == 0 then 1 + primeDivRec (div x y) y else 0 + primeDivRec x (y + 1 )
+
+primeDivisors :: Integral a => a -> a
+primeDivisors x = primeDivRec x 2
+
+plus :: Integral a => a -> a -> a
+plus x 0 = x
+plus x y = plus (succ x) (pred y)
+
+times :: Integral a => a -> a -> a
+times _ 0 = 0
+times 0 _ = 0
+times x 1 = x
+times x y = plus x (times x (y - 1))
+
+plus' :: Integral a => a -> a -> a
+plus' x 0 = x
+plus' x y = if y > 0 then plus' (succ x) (pred y) else plus' (pred x) (succ y)
+
+times' :: Integral a => a -> a -> a
+times' _ 0 = 0
+times' 0 _ = 0
+times' x y = if y > 0 then plus' x (times' x (y - 1)) else negat (times' x (negat y))
+    where negat x = -x
+
 addHead :: a -> [a] -> [a]
 addHead x y = x:y
 
@@ -83,6 +110,11 @@ getPoints :: String -> [(String, Integer)] -> Integer
 getPoints _ [] = 0
 getPoints a ((x,y):z) = if x == a then y else getPoints a z
 
+getBest :: [(String, Integer)] -> String
+getBest [] = "No one"
+getBest [(x,y)] = x
+getBest ((x,y):(a,b):z) = if y > b then getBest ((x,y):z) else getBest ((a,b):z)
+
 append :: [a] -> [a] -> [a]
 append [] y = y
 append x y = append (tail x) ((last x) : y)
@@ -111,3 +143,78 @@ add1 (x:y) = [x + 1] ++ add1 y
 multiplyN :: Num n => n -> [n] -> [n]
 multiplyN _ [] = []
 multiplyN n (x:y) = [x * n] ++ multiplyN n y
+
+deleteEven :: Integral i => [i] -> [i]
+deleteEven [] = []
+deleteEven (x:y) = if even x then deleteEven y else x : (deleteEven y)
+
+deleteElem :: Eq a => a -> [a] -> [a]
+deleteElem _ [] = []
+deleteElem a (x:y) = if x == a then deleteElem a y else x : (deleteElem a y)
+
+largestNumber :: [Integer] -> Integer
+largestNumber [x] = x
+largestNumber (x:y:z) = if x > y then largestNumber (x:z) else largestNumber (y:z)
+
+listsEqual :: Eq a => [a] -> [a] -> Bool
+listsEqual [] [] = True
+listsEqual _ [] = False
+listsEqual [] _ = False
+listsEqual (a:b) (x:y) = if a /= x then False else listsEqual b y
+
+multiplyEven :: [Integer] -> [Integer]
+multiplyEven [] = []
+multiplyEven (x:y) = if even x then (2 * x) : multiplyEven y else multiplyEven y
+
+sqroots :: [Double] -> [Double]
+sqroots [] = []
+sqroots (x:y) = if x > 0 then sqrt x : sqroots y else x : sqroots y
+
+everyNth' :: Integer -> Integer -> [a] -> [a]
+everyNth' _ _ [] = []
+everyNth' 0 n (x:y) = x : everyNth' (n - 1) n y
+everyNth' c n (_:y) = everyNth' (c - 1) n y
+
+everyNth :: Integer -> [a] -> [a]
+everyNth n x = everyNth' 0 n x
+
+brackets' :: Integer -> String -> Integer
+brackets' _ [] = 0
+brackets' n (x:y) = if n < 0 then -1 else 
+    if x == '(' then 1 + brackets' (n + 1) y else -1 + brackets' (n - 1) y
+
+brackets :: String -> Bool
+brackets a = 0 == brackets' 0 a
+
+palindrome1 :: String -> String -> String
+palindrome1 [] _ = []
+palindrome1 (x:y) [] = []
+palindrome1 (x:y) [a] = []
+palindrome1 (x:y) (a:b:[]) = [x]
+palindrome1 (x:y) (a:b:c) = palindrome1 y c ++ [x]
+
+palindrome2 :: String -> String -> String
+palindrome2 [] _ = []
+palindrome2 (x:y) [] = y
+palindrome2 (x:y) [a] = y
+palindrome2 (x:y) (a:b:[]) = y
+palindrome2 (x:y) (a:b:c) = palindrome2 y c
+
+palindrome :: String -> Bool
+palindrome x = palindrome1 x x == palindrome2 x x
+
+prefixLen :: Int -> String -> String -> Int
+prefixLen n [] [] = n
+prefixLen n x y = if x == y then n else prefixLen (n + 1) (tail x) (init y)
+
+palindromize :: String -> String
+palindromize x = x ++ (reverse (take (prefixLen 0 x (reverse x)) x))
+
+getMiddle' :: [a] -> [a] -> a
+getMiddle' (x:y) [] = x
+getMiddle' (x:y) [a] = x
+getMiddle' (x:y) (a:b:[]) = x
+getMiddle' (x:y) (a:b:c) = getMiddle' y c
+
+getMiddle :: [a] -> a
+getMiddle x = getMiddle' x x
--- ib015/Odpovedniky/odpovedniky01.hs
+++ ib015/Odpovedniky/odpovedniky01.hs
@@ -17,3 +17,20 @@ classifyIP (_, _, _, _) = "Other"
 is3DCloser :: (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> Bool
 is3DCloser (x1, y1, z1) (x2, y2, z2) (x3, y3, z3) = distance x1 y1 z1 x2 y2 z2 < distance x1 y1 z1 x3 y3 z3
     where distance x1 y1 z1 x2 y2 z2 = (x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2
+
+pointsCount :: (Integer, Integer, Integer) -> Integer
+pointsCount (x, y, z) = 5 * x + 2 * y + 3 * z
+
+moreThan4t :: Integer -> Integer
+moreThan4t x = if x >= 4 then 1 else 0
+
+lessThanSeven :: Integer -> Integer -> Integer
+lessThanSeven x y = if x - 7 <= y then 1 else 0
+
+matchPoints :: (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> (Integer, Integer)
+matchPoints (t1, c1, d1) (t2, c2, d2) =
+    if pointsCount (t1, c1, d1) == pointsCount (t2, c2, d2)
+        then (2 + moreThan4t t1, 2 + moreThan4t t2)
+        else if pointsCount (t1, c1, d1) > pointsCount (t2, c2, d2)
+            then (4 + moreThan4t t1, moreThan4t t2 + lessThanSeven (pointsCount (t1, c1, d1)) (pointsCount (t2, c2, d2)))
+            else (moreThan4t t1 + lessThanSeven (pointsCount (t2, c2, d2)) (pointsCount (t1, c1, d1)), 4 + moreThan4t t2)
