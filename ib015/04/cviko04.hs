2ff45e77c1e277e99136cfda6b572efe631aab3c
Dend0x <dannyryska05@gmail.com>
2025-10-22 14:35:46 +0200

    Update cviko04.hs

--- ib015/04/cviko04.hs
+++ ib015/04/cviko04.hs
@@ -83,3 +83,109 @@ anyEven = or . map even
 
 allEven :: [Integer] -> Bool
 allEven = and . map even
+
+blueless :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+blueless = filter (\(x,y,z) -> z == 0)
+
+greyscale :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+greyscale = filter (\(x,y,z) -> x == y && y == z)
+
+polychromatic :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+polychromatic = filter (\(x,y,z) -> (x > 0 && y > 0) || (y > 0 && z > 0) || (z > 0 && x > 0))
+
+colorsToString :: [(Int, Int, Int)] -> [String]
+colorsToString = map (\(x,y,z) -> "r: " ++ show x ++ " g: " ++ show y ++ " b: " ++ show z)
+
+quickSort :: [Integer] -> [Integer]
+quickSort [] = []
+quickSort (x:xs) = quickSort (filter (<x) xs) ++ [x] ++ quickSort (filter (>=x) xs)
+
+sumLists :: Num a => [a] -> [a] -> [a]
+sumLists xs ys = zipWith (+) xs ys
+
+upper :: String -> String
+upper xs = map toUpper xs
+
+embrace :: [String] -> [String]
+embrace xs = map ("[" ++) (map (++ "]") xs)
+
+sql :: (Ord a, Num a) => [a] -> a -> [a]
+sql xs lt = map (^2) (filter (<lt) xs)
+
+-- map even
+-- map (a -> b) -> [a] -> [b]
+-- even Num a => a -> Bool
+-- Num a => [a] -> [Bool]
+
+-- map head . snd
+-- map (f -> g) -> [f] -> [g]
+-- head [e] -> e
+-- . (b - > c) -> (a -> b) -> (a -> b -> c)
+-- snd (h, i) -> i
+-- map head [[e]] -> [e]
+-- h = [[e]]
+-- map head . snd (h, [[i]]) -> [i]
+
+-- filter ((4 >) . last)
+-- filter (a -> Bool) -> [a] -> [a]
+-- last [b] -> b
+-- (4 >) Ord c => c -> Bool
+-- . (e -> f) -> (d -> e) -> (d -> -> e -> f)
+-- ((4 >) . last) Ord b => [b] -> Bool
+-- filter ((4 >) . last) (Ord b, Num b) => [[b]] -> [[b]]
+
+-- const const
+-- const a -> b -> a
+-- const const a -> b -> c -> b
+
+countStudentsByPoints :: Integer -> [(String, Integer)] -> Int
+countStudentsByPoints n xs = length (filter (==n) (map snd xs))
+
+studentNamesByPoints :: Integer -> [(String, Integer)] -> [String]
+studentNamesByPoints n xs = getNames (filter ((==n) . snd) xs)
+
+studentsStartingWith :: Char -> [(String, Integer)] -> [(String, Integer)]
+studentsStartingWith c = filter ((==c) . head . fst)
+
+failing :: [(Int, Char)] -> [Int]
+failing = map fst . (filter ((=='F') . snd))
+
+embraceWith :: Char -> Char -> [String] -> [String]
+embraceWith l r = map ((l :) . (++[r]))
+
+divisibleBy7 :: [Integer] -> [Integer]
+divisibleBy7 = filter ((==0) . (flip mod 7))
+
+letterCaesar :: String -> String
+letterCaesar = map (chr . (+3) . ord) . filter isLetter
+
+zp :: (Integral a, Num b) => [a] -> [b] -> [b]
+zp = zipWith (flip (^))
+
+negp :: (a -> Bool) -> a -> Bool
+negp = (.) not
+
+l2c :: Char -> Int
+l2c = flip (-) (ord 'a') . ord
+
+c2l :: Int -> Char
+c2l = chr . (+) (ord 'A')
+
+lowAlphaOnly :: String -> String
+lowAlphaOnly = filter (((&&) . isLower <*> isAscii))
+
+-- isLower Char -> Bool
+-- isAscii Char -> Bool
+-- (&&) Bool -> Bool -> Bool
+-- filter (a -> Bool) -> [a] -> [a]
+-- <*> (a -> b -> c) -> (a -> b) -> a -> c
+-- (&&) isLower Char -> Bool -> Bool
+-- (&&) isLower <*> - (Char -> Bool) -> Char -> Bool
+-- ((&&) isLower <*> isAscii) -> Char -> Bool
+-- filter (((&&) isLower <*> isAscii)) -> [Char] -> [Char]
+
+--letterVigenere :: String -> String -> String
+--letterVigenere xs ks = zipWith (\x y -> c2l ((flip (mod) 26) (((+) (l2c)) (l2c)))) (lowAlphaOnly xs) (lowAlphaOnly ks)
+
+--test :: Char -> Char -> Int
+--test = (+) . l2c
