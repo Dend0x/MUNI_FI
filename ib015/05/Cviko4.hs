9be436e823b1cd9fb389c7387177951756836fb6
Dend0x <dannyryska05@gmail.com>
2025-11-04 22:45:47 +0100

    ituzvtrdx

--- ib015/05/Cviko4.hs
+++ ib015/05/Cviko4.hs
@@ -43,3 +43,72 @@ safeDiv a b = if b == 0 then Nothing else Just (div a b)
 valOrDef :: Maybe a -> a -> a
 valOrDef Nothing x = x
 valOrDef (Just a) _ = a
+
+type WeaponsPoints = Integer
+type HealthPoints = Integer
+
+data Armor = Leather | Steel -- kožené nebo ocelové brnění
+    deriving (Eq, Show)
+
+data Warrior = Warrior HealthPoints Armor WeaponsPoints
+    deriving (Eq, Show)
+
+-- Příklad hodnot Armor a Warrior - Leather, Steel
+-- Kolik hodnotových konstruktorů - Warrior, Steel, Leather
+-- Kolik typových kosntruktorů - Armor a Warrior
+
+warrior1 = Warrior 30 Leather 30
+warrior2 = Warrior 20 Steel 25
+
+attack :: Warrior -> Warrior -> Warrior
+attack (Warrior h a w) (Warrior h2 a2 w2) = if a2 == Steel then (Warrior (max (h2 - (div w 2)) 0) a2 w2) else (Warrior (max (h2 - w) 0) a2 w2)
+
+data Jar = EmptyJar | Jam String | Cucumbers | Compote Int
+
+today = 2025
+
+stale :: Jar -> Bool
+stale (Compote x) = x + 10 < today
+stale _ = False
+
+-- data X = Value Int - Typové konstruktory = X, nulární, Hodnotové konstruktory = Value, unární
+-- data M = A | B | N M - Typové konstruktory = M, nulární, Hodnotové konstruktoy = A, B, N, N je unární, jinak nulární
+-- data N = C | D | M N - Typové konstruktory = N, nulární, Hodnotové kosntruktoy =  C, D, M, M je urnární, jinak nulární
+-- data Ha = Hah Int Float [Hah] - Špatný zápis, Hah jako hodnotový konstruktor zapsán jako typový konstruktor
+-- data FMN = T (Int, Int) (Int -> Int) [Int] - typové konstruktory = FMN, nulární, Hodnotové konstruktory = T, ternární
+-- type Fat = Float -> Float -> Float - Typový alias
+-- data E = E (E, E) - typové konstruktory E, nulární, Hodnotové konstruktory = E, unární
+
+-- data N x = NVal (x -> x) - Ok
+-- type Makro = a -> a - chybí typová proměnná a
+-- data M = N (x, x) | N Bool | O M - Nelze použít hodnotový konstruktor vícekrát
+-- type Fun a = a -> (a, Bool) -> c - chybí typová proměnná c
+-- type Fun (a, c) (a, b) = (b, c) - chybný zápis typových proměnných
+-- data F = X Int | Y Float | Z X - Použití hodnotového konstruktoru jako typového
+-- data F = intfun Int - Nemá velké písmeno
+-- data F = Makro Int -> Int - Není v zárvorkách?
+-- type Val = Int | Bool - nelze dát nebo do typů
+-- data X = X X X Ok
+
+type Frac = (Int, Int)
+
+sameFrac :: Frac -> Frac -> Bool
+sameFrac (a,b) (c,d) = baseFrac (a,b) == baseFrac (c,d)
+
+nonNegFrac :: Frac -> Bool
+nonNegFrac (a,b) = a * b >= 0
+
+plusFrac :: Frac -> Frac -> Frac
+plusFrac (a,b) (c,d) = baseFrac (a * d + b * c, b * d)
+
+minusFrac :: Frac -> Frac -> Frac
+minusFrac (a,b) (c,d) = baseFrac (a * d - b * c, b * d)
+
+multFrac :: Frac -> Frac -> Frac
+multFrac (a,b) (c,d) = baseFrac (a * c, b * d)
+
+divFrac :: Frac -> Frac -> Frac
+divFrac (a,b) (c,d) = if c == 0 then (0,0) else multFrac (a,b) (d, c)
+
+baseFrac :: Frac -> Frac
+baseFrac (a,b) = ((div a (gcd a b)), (div b (gcd a b)))
--- ib015/Odpovedniky/odpovedniky03.hs
+++ ib015/Odpovedniky/odpovedniky03.hs
@@ -1,3 +1,5 @@
+import Data.List (groupBy, sortOn, nub)
+
 evalPoly :: Num a => [a] -> a -> a
 evalPoly coeffs x = foldr (\a b -> b * x + a) 0 coeffs
 
@@ -9,5 +11,32 @@ f (x:xs) (y:ys) = if x > y
     else f xs ys
 
 repeatUntil :: (a -> a) -> (a -> a -> Bool) -> a -> a
-
 repeatUntil f p start = if p start (f start) then f start else repeatUntil f p (f start)
+
+books :: [(String, String, Int)]
+books = [
+  ("The Lord of the Rings", "The Fellowship of the Ring", 1954),
+  ("The Lord of the Rings", "The Two Towers", 1954),
+  ("The Lord of the Rings", "The Return of the King", 1955),
+  ("Harry Potter", "Philosopher's Stone", 1997),
+  ("Harry Potter", "Chamber of Secrets", 1998),
+  ("Harry Potter", "Prisoner of Azkaban", 1999),
+  ("Harry Potter", "Goblet of Fire", 2000),
+  ("Harry Potter", "Order of the Phoenix", 2003),
+  ("Harry Potter", "Half-Blood Prince", 2005),
+  ("Harry Potter", "Deathly Hallows", 2007),
+  ("Mistborn: Era One", "The Final Empire", 2006),
+  ("Mistborn: Era One", "The Well of Ascension", 2007),
+  ("Mistborn: Era One", "The Hero of Ages", 2008),
+  ("Hyperion Cantos", "Hyperion", 1989),
+  ("Hyperion Cantos", "The Fall of Hyperion", 1990),
+  ("Hyperion Cantos", "Endymion", 1996),
+  ("Hyperion Cantos", "The Rise of Endymion", 1997),
+  ("The Kingkiller Chronicle", "The Name of the Wind", 2007),
+  ("The Kingkiller Chronicle", "The Wise Man's Fear", 2011)]
+
+
+selectSuitableSeries :: [(String, String, Int)] -> [String]
+selectSuitableSeries xs = nub (map (fstThree . head) (filter (\x -> length x <= 4 && all ((<=2010) . thdThree) x) (groupBy (\x y -> fstThree x == fstThree y) (sortOn fstThree xs))))
+                        where fstThree (x, _, _) = x
+                              thdThree (_, _, x) = x
